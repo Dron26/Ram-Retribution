0b2ec71375066da0d959ba9fbc155281086daac8
Dmitry <104758331+OnyxBishop@users.noreply.github.com>
2024-05-24 12:55:39 +0500

    Delete useless directory _Project

--- Assets/_Project/CodeBase/Scripts/SceneNames.cs
+++
@@ -1,8 +0,0 @@
-namespace _Project.CodeBase.Scripts
-{
-    public static class SceneNames
-    {
-        public static readonly string Lobby = nameof(Lobby);
-        public static readonly string Gameplay = nameof(Gameplay);
-    }
-}
